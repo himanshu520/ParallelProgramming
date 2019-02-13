6a6c1683ac95b767da7cc7d9a2e42ab440a1f117
Himanshu <raihimanshu520@gmail.com>
2019-02-13 21:54:16 +0530

    Implemented main function

--- Assignment-1/hare_turtle_thread.cpp
+++ Assignment-1/hare_turtle_thread.cpp
@@ -19,6 +19,8 @@ void* thread_fun(void *thread_arg) {
     int my_rank = *((int *) thread_arg);
     int sleep_time = 0;
     while(true) {
+
+        //god modifying the positions of tortoise and hare
         if(my_rank == 4) {
             if(rand() % 100 <= god_prob) {
                 cout << "God in action :-\n";
@@ -29,6 +31,7 @@ void* thread_fun(void *thread_arg) {
             }
         }
 
+        //synchronising all the four threads using condition variable
         pthread_mutex_lock(&cond_mutex);
         counter++;
         if(counter == 4) {
@@ -42,19 +45,24 @@ void* thread_fun(void *thread_arg) {
         pthread_mutex_unlock(&cond_mutex);
 
         if(my_rank == 0) {
+            //tortoise moving
             new_pos_tort += 1;
         } else if(my_rank == 1) {
+            //if hare is sleeping, then update its sleep time
             if(sleep_time > 0) sleep_time--;
             else {
-                if(old_pos_hare - old_pos_tort >= sleep_cond && rand() >= hare_sleep_prob) {
+                //if hare is not sleeping and is well ahead of tortoise, choose to sleep or not
+                if(old_pos_hare - old_pos_tort >= sleep_cond && rand() <= hare_sleep_prob) {
                     sleep_time = rand() % max_sleep_time;
                     cout << "Hare has decided to sleep for " << sleep_time + 1 << " seconds\n";
                 } else {
+                    //if hare has decided not to sleep, then it will move
                     new_pos_hare += 3;
                 }
             }
         }
 
+        //synchronising all the threads using condition variable
         pthread_mutex_lock(&cond_mutex);
         counter++;
         if(counter == 4) {
@@ -67,16 +75,38 @@ void* thread_fun(void *thread_arg) {
         }
         pthread_mutex_unlock(&cond_mutex);
 
+        //reporter printing the positions of tortoise and god
         if(my_rank == 3) {
             cout << "At time " << tim++ << "seconds :\t Hare is at " << old_pos_hare << ",\t Tortoise is at " << old_pos_tort << ".\n";
         }
 
+        //checking loop breaking condition - either tortoise or hare wins
         if(old_pos_hare >= finish_pos || old_pos_tort >= finish_pos) break;
     }
+
+    //nothing to return, hence NULL
     return NULL;
 }
 
 int main() {
+    //seeding the random time generator
     srand(time(0));
+
+    //initialising the mutex and condition variable
+    pthread_mutex_init(&cond_mutex, NULL);
+    pthread_cond_init(&cond_var, NULL);
+
+    //initialising the threads
+    pthread_t threads[4];
+    for(int i = 0; i < 4; i++)
+        pthread_create(&threads[i], NULL, &thread_fun, (void *)(&i));
+    
+    //waiting for the threads to complete
+    for(int i = 0; i < 4; i++)
+        pthread_join(&threads[i], NULL);
+
+    //printing the final result
+    print_result();
+
     return 0;
 }
