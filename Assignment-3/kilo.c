0f02614453a46692b7430ad0dfc1bd553f621b25
Himanshu <raihimanshu520@gmail.com>
2019-03-01 23:15:22 +0530

    Completed step 50

--- Assignment-3/kilo.c
+++ Assignment-3/kilo.c
@@ -14,7 +14,7 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN, PAGE_UP, PAGE_DOWN };
 
 
 /**************************************************************         data        **************************************************************/
@@ -89,11 +89,21 @@ int editorReadKey() {
         if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if(seq[0] == '[') {
-            switch(seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+            if(seq[1] >= '0' && seq[1] <= '9') {
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if(seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            } else {
+                switch(seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                }
             }
         }
 
