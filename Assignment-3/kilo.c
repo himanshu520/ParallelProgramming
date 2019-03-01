885559f1043b7a7ba89e29bf2ad2921b45da3955
Himanshu <raihimanshu520@gmail.com>
2019-03-02 01:53:25 +0530

    Completed step 79

--- Assignment-3/kilo.c
+++ Assignment-3/kilo.c
@@ -301,12 +301,22 @@ void editorRefreshScreen() {
 /**************************************************************        input        **************************************************************/
 //function to move the cursor on screen using wsad keys
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch(key) {
         case ARROW_LEFT:
             if(E.cx != 0) E.cx--;
+            else if(E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if(row && E.cx < row->size) E.cx++;
+            else if(row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
+            }
             break;
         case ARROW_UP:
             if(E.cy != 0) E.cy--;
@@ -315,6 +325,11 @@ void editorMoveCursor(int key) {
             if(E.cy < E.numrows) E.cy++;
             break;
     }
+
+    //we need to find 'row' again because our 'ey' might have changed
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if(E.cx > rowlen) E.cx = rowlen;
 }
 
 //function to process keypresses
